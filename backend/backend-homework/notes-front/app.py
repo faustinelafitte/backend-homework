d466cacd1abc2ef0694a765d713f2ac23e5fe547
Faustine Lafitte <faustine.lafitte@etu.minesparis.psl.eu>
2025-04-08 10:25:35 +0200

    etape1b

--- backend/backend-homework/notes-front/app.py
+++ backend/backend-homework/notes-front/app.py
@@ -1,43 +1,23 @@
-"""
-add a /db/alive endpoint to check if the database is alive
-"""
-VERSION = "01b"
-
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.sql import text
+
 app = Flask(__name__)
 
-## DB declaration
-
-# filename where to store stuff (sqlite is file-based)
-db_name = 'chat.db'
-# how do we connect to the database ?
-# here we say it's by looking in a file named chat.db
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + db_name
-# this variable, db, will be used for all SQLAlchemy commands
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///chat.db'
 db = SQLAlchemy(app)
 
 @app.route('/')
 def home():
     return "hello, this is a chat app!"
-# try it with
-"""
-http :5001/db/alive
-"""
+
 @app.route('/db/alive')
 def db_alive():
     try:
-        result = db.session.execute(text('SELECT 1'))
-        print(result)
-        return dict(status="healthy", message="Database connection is alive")
+        db.session.execute('SELECT 1')
+        return {"status": "ok"}
     except Exception as e:
-        # e holds description of the error
-        error_text = "<p>The error:<br>" + str(e) + "</p>"
-        hed = '<h1>Something is broken.</h1>'
-        return hed + error_text
+        return {"status": "error", "details": str(e)}, 500
 
 if __name__ == '__main__':
     app.run(debug=True)
-    
 
