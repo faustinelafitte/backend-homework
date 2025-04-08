90d6750981ed2dddb2bd2291b49b1a37466203a3
Faustine Lafitte <faustine.lafitte@etu.minesparis.psl.eu>
2025-04-08 10:43:25 +0200

    version1b

--- backend/backend-homework/notes-front/app.py
+++ backend/backend-homework/notes-front/app.py
@@ -1,9 +1,8 @@
 
-'''
-connect to a SQL (sqlite) database
-use SQLAlchemy to do so
-'''
-VERSION = "01"
+"""
+add a /db/alive endpoint to check if the database is alive
+"""
+VERSION = "01b"
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
@@ -25,6 +24,23 @@ db = SQLAlchemy(app)
 @app.route('/')
 def hello_world():
     return f'hello, this is a chat app! (version {VERSION})'
+
+
+# try it with
+"""
+http :5001/db/alive
+"""
+@app.route('/db/alive')
+def db_alive():
+    try:
+        result = db.session.execute(text('SELECT 1'))
+        print(result)
+        return dict(status="healthy", message="Database connection is alive")
+    except Exception as e:
+        # e holds description of the error
+        error_text = "<p>The error:<br>" + str(e) + "</p>"
+        hed = '<h1>Something is broken.</h1>'
+        return hed + error_text
 
 if __name__ == '__main__':
     app.run()
