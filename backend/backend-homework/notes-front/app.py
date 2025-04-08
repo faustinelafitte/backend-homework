eb204b8eeb23bb8a83786f0dbaee2f0a2f8d4ce6
Faustine Lafitte <faustine.lafitte@etu.minesparis.psl.eu>
2025-04-08 10:42:38 +0200

    version 1

--- backend/backend-homework/notes-front/app.py
+++ backend/backend-homework/notes-front/app.py
@@ -1,13 +1,26 @@
-"""
-the simplest possible hello world app
-"""
 
-VERSION = "00"
+'''
+connect to a SQL (sqlite) database
+use SQLAlchemy to do so
+'''
+VERSION = "01"
 
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql import text
 
 ## usual Flask initilization
 app = Flask(__name__)
+
+## DB declaration
+
+# filename where to store stuff (sqlite is file-based)
+db_name = 'chat.db'
+# how do we connect to the database ?
+# here we say it's by looking in a file named chat.db
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + db_name
+# this variable, db, will be used for all SQLAlchemy commands
+db = SQLAlchemy(app)
 
 @app.route('/')
 def hello_world():
