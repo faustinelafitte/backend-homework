8ca5e7f0696e93c8057a34ad9a48ab7d91f6c097
Faustine Lafitte <faustine.lafitte@etu.minesparis.psl.eu>
2025-04-08 10:37:37 +0200

    version0

--- backend/backend-homework/notes-front/app.py
+++ backend/backend-homework/notes-front/app.py
@@ -1,23 +1,17 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+"""
+the simplest possible hello world app
+"""
 
+VERSION = "00"
+
+from flask import Flask
+
+## usual Flask initilization
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///chat.db'
-db = SQLAlchemy(app)
-
 @app.route('/')
-def home():
-    return "hello, this is a chat app!"
-
-@app.route('/db/alive')
-def db_alive():
-    try:
-        db.session.execute('SELECT 1')
-        return {"status": "ok"}
-    except Exception as e:
-        return {"status": "error", "details": str(e)}, 500
+def hello_world():
+    return f'hello, this is a chat app! (version {VERSION})'
 
 if __name__ == '__main__':
-    app.run(debug=True)
-
+    app.run()
