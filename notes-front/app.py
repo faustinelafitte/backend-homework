fbc6cbc589cb0469d420c5a839ef761b84af319f
faustinelafitte <faustine.lafitte@etu.minesparis.psl.eu>
2025-04-08 10:12:27 +0200

    Add files via upload

---
+++ notes-front/app.py
@@ -0,0 +1,29 @@
+'''
+connect to a SQL (sqlite) database
+use SQLAlchemy to do so
+'''
+VERSION = "01"
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql import text
+app = Flask(__name__)
+
+## DB declaration
+
+# filename where to store stuff (sqlite is file-based)
+db_name = 'chat.db'
+# how do we connect to the database ?
+# here we say it's by looking in a file named chat.db
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + db_name
+# this variable, db, will be used for all SQLAlchemy commands
+db = SQLAlchemy(app)
+
+@app.route('/')
+def home():
+    return "hello, this is a chat app!"
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    
+
